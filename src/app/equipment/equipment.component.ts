a161c4572e62c352c59d9f32ebb477d0b3d667ba
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2022-09-01 20:26:22 -0500

    Implemented removeItem and canAdd functions.

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,29 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-   equipmentItems: object[] = [
-       {name: 'Duct Tape', mass: 0.5},
-       {name: 'Space Camera', mass: 20},
-       {name: 'Food', mass: 150},
-       {name: 'Oxygen Tanks', mass: 400},
-       {name: 'AE-35 Unit', mass: 5},
-       {name: 'ISS Supplies', mass: 800},
-       {name: 'Water', mass: 250},
-       {name: 'Satellite', mass: 1200},
-       {name: 'R2 Unit', mass: 32}
-   ];
-   cargoHold: object[] = [];
-   cargoMass: number = 0;
-   maximumAllowedMass: number = 2000;
-   maxItems: number = 10;
+  equipmentItems: object[] = [
+    { name: 'Duct Tape', mass: 0.5 },
+    { name: 'Space Camera', mass: 20 },
+    { name: 'Food', mass: 150 },
+    { name: 'Oxygen Tanks', mass: 400 },
+    { name: 'AE-35 Unit', mass: 5 },
+    { name: 'ISS Supplies', mass: 800 },
+    { name: 'Water', mass: 250 },
+    { name: 'Satellite', mass: 1200 },
+    { name: 'R2 Unit', mass: 32 }
+  ];
+  cargoHold: object[] = [];
+  cargoMass: number = 0;
+  maximumAllowedMass: number = 2000;
+  maxItems: number = 10;
 
-   constructor() { }
+  constructor() { }
 
-   ngOnInit() { }
+  ngOnInit() { }
 
-   addItem(item: object): boolean {
+  addItem(item: object): void {
     this.cargoHold.push(item);
     this.cargoMass += item['mass'];
-    return this.maximumAllowedMass - this.cargoMass < 200;
-   }   
+  }
+
+  removeItem(item: object): void {
+    this.cargoHold.splice(this.cargoHold.indexOf(item), 1);
+    this.cargoMass -= item['mass'];
+  }
+
+  canAdd(item: object): boolean {
+    return (item['mass'] + this.cargoMass <= this.maximumAllowedMass)
+      && (this.cargoHold.length < this.maxItems);
+  }
 }
